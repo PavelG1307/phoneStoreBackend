a26163f9a20fe18ee77e343ada5d5eb4c0bb0754
Pavel_prod <julu13@yandex.ru>
2023-01-23 00:00:32 +0300

    Починил корс

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: process.env.FRONT_URL || '*',
+  origin: ['192.168.1.109:3000', 'rk-tech.shop'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 };
