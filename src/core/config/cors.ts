add219dfef271e6076365b7b5d40783258eda099
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:38:25 +0300

    Исправил корс

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: process.env.FRONT_URL || '*',
+  origin: ['rk-tech.shop', '192.168.1.109:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
