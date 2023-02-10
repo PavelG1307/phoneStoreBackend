8558cf37cde0f8d9caaca12cff03cd9aa0ad02d7
Pavel_prod <julu13@yandex.ru>
2023-02-10 15:07:30 +0300

    test

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: '*',
+  origin: ['https://rk-tech.shop', 'http://192.168.1.109:3000', 'http://127.0.0.1:5500', 'https://972e-176-112-142-86.eu.ngrok.io'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'access-control-expose-headers'],
   credentials: true
