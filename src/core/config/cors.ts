bc09b56e37b42cab48aee1da26b6ee22b8cd0b24
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:14:26 +0300

    Убрал credentials header

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -3,6 +3,6 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 export const corsOptions: CorsOptions = {
   origin: process.env.FRONT_URL || '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'credentials'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
