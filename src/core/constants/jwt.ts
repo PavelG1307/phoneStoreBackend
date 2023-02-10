204bd63fd69e71485b61fcb8ea99209c09d35d16
Pavel_prod <julu13@yandex.ru>
2023-02-10 12:32:37 +0300

    Убрал Sequre cookies

--- src/core/constants/jwt.ts
+++ src/core/constants/jwt.ts
@@ -3,13 +3,13 @@ export const jwtConstants = {
     expiresIn: `7200s`,
     accessTokenOptions: {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'dev',
+      secure: false,
       maxAge: 2 * 60 * 60 * 1000,
       path: process.env.API_PREFIX,
     },
     refreshTokenOptions: {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'dev',
+      secure: false,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: process.env.API_PREFIX,
     }
