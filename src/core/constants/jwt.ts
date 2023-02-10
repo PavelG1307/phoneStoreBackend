4abc33e8ff86f9c0f311d07168ba9b7767407177
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:59:29 +0300

    Test

--- src/core/constants/jwt.ts
+++ src/core/constants/jwt.ts
@@ -2,15 +2,11 @@ export const jwtConstants = {
     secret: process.env.SECRET_JWT_KEY,
     expiresIn: '7200s',
     accessTokenOptions: {
-      httpOnly: true,
-      secure: true,
       maxAge: 2 * 60 * 60 * 1000,
       path: '/',
       samesite: 'none'
     },
     refreshTokenOptions: {
-      httpOnly: true,
-      secure: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: '/',
       samesite: 'none'
