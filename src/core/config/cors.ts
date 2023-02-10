7f088ea5d00a1727fdcb98903e1e4492b68a7886
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:47:51 +0300

    Добавил localhost в cors

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: ['rk-tech.shop', '192.168.1.109:3000'],
+  origin: ['rk-tech.shop', '192.168.1.109:3000', '127.0.0.1:5500'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
