da3fd4b81d7014aebad0f9efe42c12835c37db40
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:21:17 +0300

    Добавил  samesite: 'None'

--- src/core/constants/jwt.ts
+++ src/core/constants/jwt.ts
@@ -6,11 +6,13 @@ export const jwtConstants = {
       secure: false,
       maxAge: 2 * 60 * 60 * 1000,
       path: '/',
+      samesite: 'None'
     },
     refreshTokenOptions: {
       httpOnly: false,
       secure: false,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: '/',
+      samesite: 'None'
     }
 };
