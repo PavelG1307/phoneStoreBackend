1295692c4a4e3c1a0d1c9a30a7c6a77c11dc7092
Pavel_prod <julu13@yandex.ru>
2023-02-10 12:40:39 +0300

    Убрал httpOnly cookies

--- src/core/constants/jwt.ts
+++ src/core/constants/jwt.ts
@@ -2,13 +2,13 @@ export const jwtConstants = {
     secret: process.env.SECRET_JWT_KEY,
     expiresIn: `7200s`,
     accessTokenOptions: {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       maxAge: 2 * 60 * 60 * 1000,
       path: '/',
     },
     refreshTokenOptions: {
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: '/',
