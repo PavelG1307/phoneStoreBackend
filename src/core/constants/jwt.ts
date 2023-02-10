30016726c6e9ac8408460e1998ddcd936c27d76b
Pavel_prod <julu13@yandex.ru>
2023-02-10 13:39:46 +0300

    Сделал sequre true

--- src/core/constants/jwt.ts
+++ src/core/constants/jwt.ts
@@ -3,14 +3,14 @@ export const jwtConstants = {
     expiresIn: '7200s',
     accessTokenOptions: {
       httpOnly: true,
-      secure: false,
+      secure: true,
       maxAge: 2 * 60 * 60 * 1000,
       path: '/',
       samesite: 'none'
     },
     refreshTokenOptions: {
       httpOnly: true,
-      secure: false,
+      secure: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: '/',
       samesite: 'none'
