ca2b0439f7c3aea1a1dac4439f6a430d97d18b29
Pavel_prod <julu13@yandex.ru>
2024-01-10 18:45:47 +0300

    Добавил поле priceDependOnColor

--- src/product/dto/get-product.dto.ts
+++ src/product/dto/get-product.dto.ts
@@ -33,3 +33,47 @@ export class GetProductDto {
   @IsString({ each: true })
   readonly productUUIDs?: string[]
 }
+
+export class GetProductsResponseItemDto {
+  uuid: string
+  name: string
+  price: number
+  priceOld?: number
+  categoryUUID: UUID
+  images?: string[]
+  description?: string
+  visible?: boolean
+  options?: OptionList[]
+  variants?: Variant[]
+  priceDependOnColor?: boolean
+  isDeleted?: boolean
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+class Option {
+  id: number
+  name?: string
+  values: string
+  type?: string
+}
+
+class OptionList {
+  name: string
+  items: Option[]
+}
+
+class Variant {
+  id: number
+  isDefault?: boolean
+  optionIds: number[]
+  optionInfo: VariantOptionInfo
+}
+
+class VariantOptionInfo {
+  name?: string
+  price?: number
+  priceOld?: number
+  images?: string[]
+  description?: string
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,9 +2,10 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards, Vali
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { Product } from '../models/product.model';
 import { CreateProductDto } from './dto/create-product.dto';
-import { GetProductDto } from './dto/get-product.dto';
+import { GetProductDto, GetProductsResponseItemDto } from './dto/get-product.dto';
 import { ProductService } from './product.service';
-import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiCookieAuth, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @ApiTags('Product')
 @Controller('product')
@@ -12,31 +13,35 @@ export class ProductController {
   constructor(private readonly ProductService: ProductService) { }
 
   @Get(':uuid')
-  async get(@Param('uuid') uuid: string) {
+  @ApiParam({ name: 'uuid', description: 'UUID товара', format: "uuid", type: "string" })
+  async get(@Param('uuid') uuid: string): Promise<GetProductsResponseItemDto> {
     return this.ProductService.get(uuid)
   }
 
   @Get()
-  async getAll(@Query(new ValidationPipe({ transform: true })) filters: GetProductDto) {
+  async getAll(@Query(new ValidationPipe({ transform: true })) filters: GetProductDto): Promise<GetProductsResponseItemDto[]> {
     return this.ProductService.getAll(filters)
   }
 
   @Post()
   @ApiCookieAuth()
   @UseGuards(JwtAuthGuard)
-  async create(@Body() product: CreateProductDto) {
+  async create(@Body() product: CreateProductDto): Promise<GetProductsResponseItemDto> {
     return this.ProductService.create(product)
   }
 
   @Put(':uuid')
   @ApiCookieAuth()
+  @ApiParam({ name: 'uuid', description: 'UUID товара', format: "uuid", type: "string", })
+  @ApiBody({ type: UpdateProductDto })
   @UseGuards(JwtAuthGuard)
-  async update(@Param('uuid') uuid: string, @Body() product: Partial<Product>) {
+  async update(@Param('uuid') uuid: string, @Body() product: UpdateProductDto): Promise<GetProductsResponseItemDto> {
     return this.ProductService.update(uuid, product)
   }
 
   @Delete(':uuid')
   @ApiCookieAuth()
+  @ApiParam({ name: 'uuid', description: 'UUID товара', format: "uuid", type: "string" })
   @UseGuards(JwtAuthGuard)
   async delete(@Param('uuid') uuid: string) {
     return this.ProductService.delete(uuid)
